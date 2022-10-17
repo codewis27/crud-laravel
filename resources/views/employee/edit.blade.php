7084fb13bbf5f31c191a1332717fa1e67ec4cbe9
unknown <aguswisma27@gmail.com>
2022-10-17 15:36:48 +0800

    ubah baru

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Edit Employee Data</h3>
+                <h3 class="card-title">Edit Employee Data || Test</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
